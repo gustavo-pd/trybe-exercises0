5b7394b1a621b6cec48327388016bde1558785ef
GustavoPD <gudalmoro@gmail.com>
2021-06-01 17:52:00 -0300

    exercicio terminado

---
+++ bloco04/04.01/scripts1.js
@@ -0,0 +1,15 @@
+const name = "Gustavo";
+var birthCity = "Passo Fundo";
+var birthYear = 1992;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+var birthYear = 2030;
+
+console.log(birthYear);
+
+var birthCity = "Erechim";
+
+console.log(birthCity);
