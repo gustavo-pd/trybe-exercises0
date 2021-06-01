0140d21bb294d8ce0fe0513901d8998a2b6733b1
GustavoPD <gudalmoro@gmail.com>
2021-06-01 17:40:05 -0300

    Terminei o exercicio feito no video(scripts.js) e estou criando o de pratica(scripts1.js)!

---
+++ bloco04/04.01/scripts.js
@@ -0,0 +1,9 @@
+let age = 20;
+age = 30;
+let fullName = "Hulk Glessias";
+
+console.log(age);
+console.log(fullName);
+
+let ultima = "Idade: " + age + "  Nome completo: " + fullName;
+console.log(ultima);
